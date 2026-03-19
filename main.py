c6ee05b7ec9af07f5455ae2bb1e90b9a8a157813
Shadow-code-dev <utsabbose2@gmail.com>
2026-03-20 00:57:19 +0530

    Add PDF text extraction using pypdf

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI, File, UploadFile
 import shutil
 from typing import BinaryIO
+from pypdf import PdfReader
+
+def extract_text(pdf_path: str) -> str:
+    reader = PdfReader(pdf_path)
+    text = ""
+
+    for page in reader.pages:
+        if page.extract_text():
+            text += page.extract_text()
+    return text
 
 app = FastAPI()
 
@@ -15,4 +25,8 @@ def upload_file(file: UploadFile = File(...)):
     with open(file_path, "wb") as buffer: # type: BinaryIO
         shutil.copyfileobj(file.file, buffer)
 
-    return {"filename": file.filename}
+    text = extract_text(file_path)
+
+    return {"filename": file.filename,
+            "text_preview": text[:500] # first 500 chars
+            }
